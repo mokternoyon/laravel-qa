dcee7e015273935382dbf60ad49152219bf66bbe
mokternoyon <mokterahmednoyon@gmail.com>
2020-10-22 22:33:13 +0600

    Extend the form functionality to be able update question

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -40,7 +40,7 @@ class QuestionsController extends Controller
     public function store(AskQuestionRequest $request)
     {
         $request->user()->questions()->create($request->only('title','body'));
-        return redirect()->route('questions.index')->with('success','Your question ha en submitted');
+        return redirect()->route('questions.index')->with('success','Your question been submitted');
     }
 
     /**
@@ -62,7 +62,7 @@ class QuestionsController extends Controller
      */
     public function edit(Question $question)
     {
-        //
+        return view('questions.edit',compact('question'));
     }
 
     /**
@@ -72,9 +72,10 @@ class QuestionsController extends Controller
      * @param  \App\Question  $question
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Question $question)
+    public function update(AskQuestionRequest $request, Question $question)
     {
-        //
+        $question->update($request->only('title','body'));
+        return redirect()->route('questions.index')->with('success','Your question been updated');
     }
 
     /**
